52d0c15a634528734895ebe3b0ef6b086f7ef566
Koen MHS <koen@mind-heart-soul.org>
2021-06-15 19:34:26 +0200

    grafana repo fix

--- _individual_installers/common_grafana.sh
+++ _individual_installers/common_grafana.sh
@@ -18,7 +18,7 @@ source $dir/common_variables.conf
 apt-get update
 apt-get install -y apt-transport-https software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
-echo "deb https://packages.grafana.com/enterprise/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
 
 # install grafana
 apt-get install -y grafana
