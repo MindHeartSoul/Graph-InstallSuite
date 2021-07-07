28d5e0ccceead133f91c4d191171ae443624a07d
Koen MHS <koen@mind-heart-soul.org>
2021-07-07 02:08:18 +0200

    postgres - apt update before install

--- _individual_installers/mainnet_1_postgres.sh
+++ _individual_installers/mainnet_1_postgres.sh
@@ -24,6 +24,7 @@ apt-get update
 apt-get install -y wget sudo
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update
 apt-get install -y postgresql-12
 
 # prepare postgres
